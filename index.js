26d96b15e2137a19668810482ec71d89c08e44de
Germán Rodríguez <germanrodriguez@MacBook-Pro-de-German.local>
2018-10-05 20:02:14 +0200

    configuration react

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = document.getElementById('app');
+
+
+const helloWorld = <h1>Hello World!!</h1>
+
+ReactDOM.render(helloWorld, app);
